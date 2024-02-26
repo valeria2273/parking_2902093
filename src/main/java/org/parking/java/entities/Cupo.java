d7213010c679d13962b35d9fb238f4122cc7d730
yeison <you@example.com>
2024-02-26 14:22:23 -0500

    Entidades listas para el main

--- src/main/java/org/parking/java/entities/Cupo.java
+++ src/main/java/org/parking/java/entities/Cupo.java
@@ -1,5 +1,14 @@
 package org.parking.java.entities;
 
 public class Cupo {
+    public char nombre;
 
+    public Cupo() {
+    }
+
+    public Cupo(char nombre) {
+        this.nombre = nombre;
+    }
+
+    
 }
--- src/main/java/org/parking/java/entities/Pago.java
+++ src/main/java/org/parking/java/entities/Pago.java
@@ -1,5 +1,38 @@
 package org.parking.java.entities;
+import java.time.LocalDateTime;
 
 public class Pago {
 
+    //Atributos Intrinsecos
+    public LocalDateTime fechaHoraInicio;
+    public LocalDateTime fechaHoraFin;
+    public Double valor;
+
+    //Atributo de relación
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+
+
+    //Constructor por defecto     
+    public Pago() {
+    }
+
+
+    //Constructor parametrizado
+    public Pago(LocalDateTime fechaHoraInicio, 
+                LocalDateTime fechaHoraFin, 
+                Double valor, 
+                Vehiculo vehiculo, 
+                Cupo cupo) {
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+
+
+    
 }
+
+
